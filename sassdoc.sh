89146e8400c05e27bc63d9ba04fc9c8ead578474
Yvain Liechti <yliechti@clever-age.com>
2015-12-13 15:47:48 +0100

    src

---
+++ sassdoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Generate documentation and deploy it to GitHub pages
+# http://sass-mq.github.io/sass-mq/
+sassdoc scss sassdoc --config=.sassdocrc
+git add sassdoc
+git commit -m "Compile SassDoc"
+git subtree push --prefix sassdoc origin gh-pages
